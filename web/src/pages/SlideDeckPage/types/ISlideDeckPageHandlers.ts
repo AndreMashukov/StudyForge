4d5e917febec1743567ec12f849a108b671f704a
Andrey <andrey-mac@Andreys-Mac-mini.local>
2026-05-22 09:42:54 +0800

    Add native fullscreen for slide decks

--- web/src/pages/SlideDeckPage/types/ISlideDeckPageHandlers.ts
+++ web/src/pages/SlideDeckPage/types/ISlideDeckPageHandlers.ts
@@ -1,10 +1,8 @@
 export interface ISlideDeckPageHandlers {
   currentSlide: number;
-  isFullscreen: boolean;
   handleNavigateBack: () => void;
   handleSlideChange: (index: number) => void;
   handlePrevSlide: () => void;
   handleNextSlide: (maxIndex: number) => void;
   handleClampSlide: (lastIndex: number) => void;
-  handleToggleFullscreen: () => void;
 }
